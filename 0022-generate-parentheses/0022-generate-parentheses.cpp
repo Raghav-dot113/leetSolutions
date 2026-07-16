663587263e7e7b7b091a70a3946c30f1e4d4bcd8
Raghav-dot113 <gargraghav138@gmail.com>
2026-07-16 19:35:44 +0530

    Time: 1417 ms (5.02%), Space: 261.4 MB (5%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    set<string> set;
+    void dfs(string cur, int remaining){
+    if(remaining == 0){
+        set.insert(cur);
+        return;
+    }
+
+    for (int i = 0; i <= cur.size(); i++) {
+    string next = cur;
+    next.insert(i, "()");
+    dfs(next, remaining - 1);
+}
+}
+    vector<string> generateParenthesis(int n) {
+        if(n == 1) return {"()"};
+        dfs("()",n-1);
+       vector<string> ans;
+       for(auto i : set) ans.push_back(i);;
+       return ans;
+    }
+};
