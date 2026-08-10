27dd3f0cf13968bb13a38895d8e50aa803e24313
Raghav-dot113 <gargraghav138@gmail.com>
2026-08-10 19:05:26 +0530

    Time: 0 ms (100%), Space: 14.1 MB (65.6%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void f(vector<int>& nums, int tar, int idx, vector<int>& curr,
+            vector<vector<int>>& ans) {
+
+        if(tar == 0) {
+            ans.push_back(curr);
+            return;
+        }
+
+        if(tar < 0 || idx == nums.size()) {
+            return;
+        }
+
+        for(int i = idx; i < nums.size(); i++) {
+
+            // Skip duplicate choices at this level
+            if(i > idx && nums[i] == nums[i - 1])
+                continue;
+
+            // Since sorted, no later value can work
+            if(nums[i] > tar)
+                break;
+
+            curr.push_back(nums[i]);
+
+            f(nums, tar - nums[i], i + 1, curr, ans);
+
+            curr.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& nums, int tar) {
+
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> ans;
+        vector<int> curr;
+
+        f(nums, tar, 0, curr, ans);
+
+        return ans;
+    }
+};
