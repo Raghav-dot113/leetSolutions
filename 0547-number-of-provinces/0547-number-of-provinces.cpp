c5991bd693fd882fc87ec7724844f2fac54e6a6c
Raghav-dot113 <gargraghav138@gmail.com>
2026-09-07 09:59:17 +0530

    Time: 0 ms (100%), Space: 19.2 MB (92.35%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
-    void DFSH(int u,vector<bool> &vis,vector<vector<int>>& adj){
-        vis[u] = true;
+    void DFSH(int u,vector<bool>& vis,vector<vector<int>>& isConnected){
+        vis[u]=true;
 
-        for(int v : adj[u-1]){
-            if(!vis[v]) DFSH(v,vis,adj);
+        for(int v=0;v<isConnected.size();v++){
+            if(isConnected[u][v]==1 && !vis[v]){
+                DFSH(v,vis,isConnected);
+            }
         }
     }
 
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        vector<vector<int>> adj;
+    int findCircleNum(vector<vector<int>>& isConnected){
+        int n=isConnected.size();
+        vector<bool> vis(n,false);
+        int count=0;
 
-        int n = isConnected.size();
-
-        for(int i = 0;i<n;i++){
-            vector<int> neigh;
-            for(int j = 0;j<n;j++){
-                if(i != j){
-                    if(isConnected[i][j] == 1){
-                        neigh.push_back(j+1);
-                    }
-                }
-            }
-
-            adj.push_back(neigh);
-        }
-
-        int count = 0;
-
-        vector<bool> vis(n+1,false);
-
-        for(int i =1;i<=n;i++){
-            if(!vis[i]) {
-            DFSH(i,vis,adj);
-            count++;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                DFSH(i,vis,isConnected);
+                count++;
             }
         }
 
